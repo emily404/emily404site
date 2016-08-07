7c0745ae8898098ffcebeafbf00f354de65c80db
emily404 <emily-404@hotmail.com>
2016-08-07 11:35:50 +0200

    wookmark twice

--- public/js/projectTiles.js
+++ public/js/projectTiles.js
@@ -1,14 +1,23 @@
-$('#projectList').imagesLoaded()
-			.always( function() {
-				var wookmark = new Wookmark('#projectList', {
-					flexibleWidth: true,
-					offset: 40,
-					itemWidth: 200
-				});
+ $(document).ready(function() {
+ 	$('#projectList').imagesLoaded()
+		.always( function() {
+			var options = {
+				autoresize: true,
+				flexibleWidth: true,
+				offset: 40,
+				itemWidth: 200
+			}
+
+			var wookmark = new Wookmark('#projectList', options);
+
+			$(window).load(function() {
+	   			handler.wookmark(options);
+			});
+		})
+		.done( function( instance ) {
+			console.log('all project list images successfully loaded');
 			})
-			.done( function( instance ) {
-	    		console.log('all project list images successfully loaded');
-	  		})
-		    .fail( function() {
-    			console.log('at least one project list image is broken');
-  			});
+	    .fail( function() {
+			console.log('at least one project list image is broken');
+			});
+});
